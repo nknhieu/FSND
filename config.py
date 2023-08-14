bcec5d841590a02df7a6e0707c5f2d1152bc3351
nkenhieu <kenhieuit@gmail.com>
2023-08-14 11:31:52 +0700

    fix bug4

--- config.py
+++ config.py
@@ -7,9 +7,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+database_name = "capstone"
+# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = "postgres:///{}".format(database_name)
+database_path = os.environ['DATABASE_URL']
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0000@localhost:5432/myFSND'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0000@localhost:5432/myFSND'
+QLALCHEMY_DATABASE_URI = database_path
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 import datetime
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 db = SQLAlchemy()
